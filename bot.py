e673acccd400afbfa2a863d7a3ba093d44eaac60
ig6268 <32320724+ig6268@users.noreply.github.com>
2020-06-24 16:54:12 +0900

    token keeping secret

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ driver.implicitly_wait(3)
 client = discord.Client()
 
 # 1-6에서 생성된 토큰을 이곳에 입력해주세요.
-token = 'NDc3NzMzMjgzMTk1NzgxMTMw.DlAbow.q8aqeiDpfYk4vAPr9169eZis8xo'
+token = '########################################'
 
 # 봇이 구동되었을 때 동작되는 코드입니다.
 @client.event
